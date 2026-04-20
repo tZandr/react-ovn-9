bd149a40fb6f34b5522f8888a03b9e72db61013f
tZandr <alex_tjernstrom@hotmail.com>
2026-04-20 16:07:33 +0200

    pages

---
+++ src/pages/AddItem.tsx
@@ -0,0 +1,10 @@
+import AddItemForm from "../components/AddItemForm";
+
+export default function AddItem() {
+    return (
+        <div>
+            <h1>Add Item</h1>
+            <AddItemForm />
+        </div>
+    )
+}
